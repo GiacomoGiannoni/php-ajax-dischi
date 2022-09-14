b968c0110276c5c7e1812ecea8ff59abc68a1513
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-09-14 14:27:39 +0200

    header php version complete

---
+++ primaVersione/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Dischi</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js" ></script>
+        <link rel="stylesheet" href="../styles/style.css">
+    </head>
+    <body>
+        <header class="my_header container-fluid py-2 px-4 d-flex justify-content-between">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="spotify logo">
+        </header>
+
+        <main>
+
+        </main>
+    </body>
+</html>
