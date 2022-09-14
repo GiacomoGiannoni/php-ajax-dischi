33d8d81eee4f828f014aae21c883ffca52a88a0c
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-09-14 14:52:18 +0200

    creata la parte di js

---
+++ secondaVersione/js/main.js
@@ -0,0 +1,20 @@
+const app = new vue({
+    el: '#app',
+    data: {
+        albums: [],
+    },
+    methods: {
+        getAlbums: function() {
+            axios.get('http://localhost/php-ajax-dischi/SecondaVersione/controller.php')
+            .then(response => {
+                this.albums = response.data;
+                console.log(this.albums);
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    },
+    created: function() {
+        this.getAlbums();
+    }
+});
